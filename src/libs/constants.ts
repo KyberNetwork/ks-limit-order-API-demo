c5b5197040d3af088dad6a4241f80bba67c43a11
Aw Kai Shin <kaishinaw@Aws-MacBook-Pro.local>
2023-10-24 09:31:24 +0800

    Add chainIds

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -10,25 +10,26 @@ interface Token {
 
 export enum ChainId {
     MAINNET = 1,
-    GÖRLI = 5,
+    BSC = 56,
+    ARBITRUM = 42161,
     MATIC = 137,
+    OPTIMISM = 10,
+    AVAX = 43114,
+    BASE = 8453,
+    CRONOS = 25,
+    ZKSYNC = 324,
+    FANTOM = 250,
+    LINEA = 59144,
+    POLYGONZKEVM = 1101,
+    AURORA = 1313161554,
+    BTTC = 199,
+    ZKEVM = 1101,
+    SCROLL = 534352,
+    // testnets
+    GÖRLI = 5,
     MUMBAI = 80001,
     BSCTESTNET = 97,
-    BSCMAINNET = 56,
     AVAXTESTNET = 43113,
-    AVAXMAINNET = 43114,
-    FANTOM = 250,
-    CRONOS = 25,
-    ARBITRUM = 42161,
-    BTTC = 199,
-    AURORA = 1313161554,
-    OPTIMISM = 10,
-    ZKSYNC = 324,
-    LINEA = 59144,
-    ZKEVM = 1101,
-    BASE = 8453,
-    SOLANA = 101, //fake id
-    SOLANA_DEVNET = 103, //fake id
   };
 
 export const takerAsset: Token = {
